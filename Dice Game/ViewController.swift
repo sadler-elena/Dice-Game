21d61c968f464983d2cc1c4d20f3880312a32429
Elena <elena_sadler@yahoo.com>
2020-12-17 14:02:17 -0600

    Initial commit

---
+++ Dice Game/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Dice Game
+//
+//  Created by Elena Sadler on 12/17/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    //This is where we will place the 'outlets' which are linked to the dice views in the storyboard. It allows the code to access their value and change them. They are similar to variables.
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    //This is where we will place our function that is connected to the button on the storyboard. The function will describe what we want to happen when the 'roll' button is pushed by the user.
+        //To access the dice images that are being displayed:
+        //(name of the object you are trying to change).(what quality of the object you want to change) = (the new value you want to set the object to)
+        //Basically, who.what = value
+        //Ex: diceImageView1.image = diceImage[randomNumber]
+    
+
+}
+
